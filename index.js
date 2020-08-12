bca66ab62f6d1443c5bc4392e340111c7c0ca241
AgungMubarok <agungmubarok555@gmail.com>
2020-08-12 09:08:34 +0700

    first

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const express = require('express')
 const app = express()
 
 // setup view engine with ejs
+app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    let text = "Hello world";
+    let text = "Data Siswa";
     let dataSiswa = [
-        {nama: "Dimas", age: 19},
-        {nama: "Rumondang", age: 20},
-        {nama: "Chris", age: 21},
+        {id: 1, nama: "Dimas", age: 19},
+        {id: 2, nama: "Rumondang", age: 20},
+        {id: 3, nama: "Chris", age: 21},
     ]
 
     res.render('pages/home', {
@@ -20,7 +21,20 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.render('pages/about')
+    let text2 = "Biodata siswa";
+    let biodata = {
+        fullname: 'agung mubarok',
+        name: 'agung',
+        age: 19,
+        address: 'Bogor',
+    }
+    let hobby = ['sleep', 'week up']
+
+    res.render('pages/about', {
+        text2: text2,
+        biodata: biodata,
+        hobby: hobby,
+    })
 })
 
 app.listen(5000, () => {
